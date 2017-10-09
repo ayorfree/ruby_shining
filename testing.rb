0d63d60725916381dcaf0528712cb664122a38a8
zhangjiyun <ayorfree@gmail.com>
2017-10-09 19:40:23 +0800

    t..t..testing

--- testing.rb
+++ testing.rb
@@ -1,24 +1,69 @@
-class Person
-  @@no_of_person = 0
-  def initialize(id, name, job)
-    @id = id
-    @name = name
-    @job = job
+
+$like = "i like eat this friut"
+VAL = "50KG"
+
+class Apple
+  VAL = "100KG"
+  @@no_of_types = 0
+  def initialize(type, price, place)
+    @type = type
+    @price = price
+    @place = place
   end
   def describe_details
-    puts "this man is called #@name, id is #@id, work is #@job"
+    puts "This apple is type of #@type and price is #@price¥/kg and comes from #@place"
   end
-  def no_of_person
-    @@no_of_person += 1
-    puts "Total Number of Person is #@@no_of_person"
+  def total_types_of_apples
+    @@no_of_types += 1
+    puts "Total types of apples is #@@no_of_types"
+  end
+  def like
+    puts "#{$like}"
+  end
+  def weight
+    puts "Total weight is #{VAL}"
   end
 end
 
-person1 = Person.new("1", "zhangjiyun", "it")
-person2 = Person.new("2", "huangxuan", "server")
 
-person1.describe_details
-person1.no_of_person
+yellowcow = Apple.new("yellowcow", 10, "tianshui")
+richperson = Apple.new("richperson", 8, "zibo")
+snakefriut = Apple.new("snakefriut", 5, "losangel")
 
-person2.describe_details
-person2.no_of_person
+yellowcow.describe_details
+yellowcow.total_types_of_apples
+
+richperson.describe_details
+richperson.total_types_of_apples
+
+snakefriut.describe_details
+snakefriut.total_types_of_apples
+
+yellowcow.like
+
+class Banana
+  VAL = "200KG"
+  def like
+    puts "#$like"
+  end
+  def weight
+    puts "total weight of banana is #{VAL}"
+  end
+end
+
+banana = Banana.new
+banana.like
+
+yellowcow.weight
+banana.weight
+
+
+puts defined? $like
+puts defined? banana.weight
+
+puts "#{VAL}"
+
+puts "#{Apple::VAL}"
+puts "#{Banana::VAL}"
+
+
